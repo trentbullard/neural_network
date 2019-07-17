1709b3ad590cdc0ad3f27792f69e59cf0b890abc
Trent Bullard <trent.bullard@onit.com>
2019-07-17 08:33:24 -0500

    implement simple nn

---
+++ main.py
@@ -0,0 +1,50 @@
+# input layer, x
+# hidden layers (arbitrary number)
+# output layer, ŷ
+# weights, W
+# biases, b
+# activation function for hidden layers, σ (e.g. Sigmoid activation function)
+# 
+# output ŷ of a 2-layer Neural Network:
+# ŷ = σ ( W₂σ( W₁x + b₁ ) + b₂ )
+
+import numpy as np
+
+def sigmoid(x):
+    return 1.0/(1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    return x * (1.0 - x)
+
+class NeuralNetwork:
+    def __init__(self, x, y):
+        self.input = x
+        self.weights1 = np.random.rand(self.input.shape[1],4)
+        self.weights2 = np.random.rand(4,1)
+        self.y = y
+        self.output = np.zeros(y.shape)
+
+    def feedforward(self):
+        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
+        self.output = sigmoid(np.dot(self.layer1, self.weights2))
+
+    def backprop(self):
+        d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
+        d_weights1 = np.dot(self.input.T, (np.dot(2*(self.y - self.output)* sigmoid_derivative(self.output), self.weights2.T)* sigmoid_derivative(self.layer1)))
+
+        self.weights1 += d_weights1
+        self.weights2 += d_weights2
+
+if __name__ == "__main__":
+    X = np.array([[0,0,1],
+                  [0,1,1],
+                  [1,0,1],
+                  [1,1,1]])
+    y = np.array([[0],[1],[1],[0]])
+    nn = NeuralNetwork(X,y)
+
+    for i in range(1500):
+        nn.feedforward()
+        nn.backprop()
+
+    print(nn.output)
